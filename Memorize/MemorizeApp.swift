b7c58a8c7ee7e396e22e0ec0088d6f62da724ffb
Kevan Barsky <k.barsky@illuscio.com>
2024-03-27 16:03:07 -0700

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Kevan Barsky on 3/27/24.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
